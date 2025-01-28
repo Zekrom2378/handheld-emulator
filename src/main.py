c4e61073f2a547c4d86a5f531c523930646c35fa
Zekrom2378 <tanner.hart@yahoo.com>
2025-01-28 17:06:35 -0500

    Created the theme menu in the settings, should all be working as expected though the vertical navigation in it is a bit weird due to the same functions being used elsewhere.

--- src/main.py
+++ src/main.py
@@ -34,7 +34,9 @@ THEMES = [["Samurott",      "#0F3057",     "#FFD166",       "#3A5F78",       "#A
           ["Zoroark",       "#2A2A2A",     "#F0F0F0",       "#5E5E5E",       "#9E2A2A",    "#D1D1D1",  "#7C2B2B",   "#9E4A4A"],
           ["Diglett",       "#5D3D2E",     "#F0E8C0",       "#6B4A2D",       "#B48433",    "#F0E8C0",  "#7A5C41",   "#9E7B54"],
           ["Zekrom",        "#0D0D0D",     "#E0E0E0",       "#1F2833",       "#0B8FAC",    "#76D7EA",  "#292929",   "#3D3D3D"],
-          ["Reshiram",      "#F8F8F8",     "#3B1C14",       "#FAE6C8",       "#FF9F33",    "#D96629",  "#E3E3E3",   "#CFCFCF"]
+          ["Reshiram",      "#F8F8F8",     "#3B1C14",       "#FAE6C8",       "#FF9F33",    "#D96629",  "#E3E3E3",   "#CFCFCF"],
+          ["Galvantula",    "#2A2D45",     "#F8F8F8",       "#3B3E5E",       "#FFA500",    "#1B1B1B",  "#FFD200",   "#FFEE70"],
+          ["Druddigon",     "#263F58",     "#F1F1F1",       "#2F3F56",       "#D2504E",    "#F8F8F8",  "#9A1F26",   "#B04D4F"]
           ]
 
 
@@ -93,14 +95,16 @@ def number_of_rows_on_page():
     if DISPLAY_STATE == 2:
         rows = 3
     if DISPLAY_STATE == 3:
+        rows = 12
+    if DISPLAY_STATE == 4:
+        pass
+    if DISPLAY_STATE == 5:
         pass
     return rows
 
 
 def focus_next():
-    global PAGE
     global ROW_NUMBER
-    global DISPLAY_STATE
     max_rows = number_of_rows_on_page()
 
     if ROW_NUMBER == max_rows - 1:
@@ -111,9 +115,7 @@ def focus_next():
 
 
 def focus_previous():
-    global PAGE
     global ROW_NUMBER
-    global DISPLAY_STATE
     max_rows = number_of_rows_on_page()
 
     if ROW_NUMBER == 0:
@@ -123,6 +125,26 @@ def focus_previous():
     if not BUTTONS[ROW_NUMBER].focus_get() in BUTTONS:
         ROW_NUMBER = max_rows - 1
     BUTTONS[ROW_NUMBER].focus_set()
+
+
+def focus_left():
+    global ROW_NUMBER
+    max = number_of_rows_on_page()
+    if ROW_NUMBER < 2:
+        ROW_NUMBER += max - 2
+    else:
+        ROW_NUMBER -= 2
+    focus_previous()
+
+
+def focus_right():
+    global ROW_NUMBER
+    max = number_of_rows_on_page()
+    if ROW_NUMBER >= max - 2:
+        ROW_NUMBER -= max - 2
+    else:
+        ROW_NUMBER += 2
+    focus_next()
 
 
 def quick_select_button(button):
@@ -187,11 +209,19 @@ def goto_time():
 
 
 def goto_themes():
-    pass
+    theme_setting_page()
 
 
 def goto_calibrate():
     pass
+
+
+def set_theme():
+    thing = BUTTONS[0].focus_get()
+    for i in range(len(BUTTONS)):
+        if BUTTONS[i] == thing:
+            theme_writer(i)
+            theme_setting_page()
 
 
 def game_display_page(page_num):
@@ -348,9 +378,49 @@ def theme_setting_page():
     root.unbind("<Down>")
     frm = tk.Frame(root, bg=THEMES[THEME_NUMBER][1])
     frm.grid(row=0, column=0, sticky="nsew")
+    frm.columnconfigure(0, weight=1)
+    frm.columnconfigure(2, weight=1)
+    frm.columnconfigure(4, weight=1)
+    frm.columnconfigure(6, weight=1)
+    frm.columnconfigure(8, weight=1)
+    frm.rowconfigure(0, weight=1)
+    frm.rowconfigure(8, weight=1)
 
-    root.bind("<Left>", lambda event: focus_previous())
-    root.bind("<Right>", lambda event: focus_next())
+    BUTTONS.clear()
+
+    # SPACERS
+    for i in range(9):
+        if i % 2 == 0:
+            tk.Label(frm, text='___', foreground=THEMES[THEME_NUMBER][1], background=THEMES[THEME_NUMBER][1]).grid(row=3, column=i)
+            tk.Label(frm, text='___', foreground=THEMES[THEME_NUMBER][1], background=THEMES[THEME_NUMBER][1]).grid(row=5, column=i)
+            tk.Label(frm, text='___', foreground=THEMES[THEME_NUMBER][1], background=THEMES[THEME_NUMBER][1]).grid(row=7, column=i)
+    tk.Label(frm, text="_", foreground=THEMES[THEME_NUMBER][1], background=THEMES[THEME_NUMBER][1]).grid(row=2, columnspan=9)
+    tk.Label(frm, text="_", foreground=THEMES[THEME_NUMBER][1], background=THEMES[THEME_NUMBER][1]).grid(row=4, columnspan=9)
+    tk.Label(frm, text="_", foreground=THEMES[THEME_NUMBER][1], background=THEMES[THEME_NUMBER][1]).grid(row=6, columnspan=9)
+    tk.Label(frm, text="_", foreground=THEMES[THEME_NUMBER][1], background=THEMES[THEME_NUMBER][1]).grid(row=8, columnspan=9)
+
+    for i in range(number_of_rows_on_page()):
+        butt = cb.SelectionButton(frm, text=THEMES[i][0], font=("Terminal", 10), command=set_theme, anchor="center")
+        butt.grid(row=((i % 3) * 2) + 3, column=((i // 3) * 2) + 1, sticky="ew")
+        BUTTONS.append(butt)
+
+    home_button = cb.MenuButton(frm, text="[<-", command=goto_settings, padx=2, pady=2)
+    home_button.config(padx=0, pady=0, font=("System", 18))
+    home_button.grid(row=8, column=0, sticky="sw", padx=3, pady=1)
+
+    invis_button = cb.MenuButton(frm, text="[<-", command=None, padx=2, pady=2)
+    invis_button.config(padx=0, pady=0, relief=tk.FLAT, background=THEMES[THEME_NUMBER][1], font=("System", 18),
+                        activebackground=THEMES[THEME_NUMBER][1], foreground=THEMES[THEME_NUMBER][1],
+                        activeforeground=THEMES[THEME_NUMBER][1])
+    invis_button.grid(row=8, column=8, sticky="se", padx=3, pady=1)
+
+    root.bind("<Left>", lambda event: focus_left())
+    root.bind("<Right>", lambda event: focus_right())
+    root.bind("<Up>", lambda event: focus_previous())
+    root.bind("<Down>", lambda event: focus_next())
+    root.bind("<b>", lambda event: goto_settings())
+
+    BUTTONS[0].focus_set()
 
 
 def time_setting_page():
